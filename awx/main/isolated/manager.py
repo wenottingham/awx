2e183eb2d5d54db784011abb6ea93a6eb44e28e1
Bill Nottingham <notting@splat.cc>
2019-12-10 23:00:03 -0500

    nope

--- awx/main/isolated/manager.py
+++ awx/main/isolated/manager.py
@@ -22,7 +22,6 @@ playbook_logger = logging.getLogger('awx.isolated.manager.playbooks')
 
 
 def set_pythonpath(venv_libdir, env):
-    return
     env.pop('PYTHONPATH', None)  # default to none if no python_ver matches
     for version in os.listdir(venv_libdir):
         if fnmatch.fnmatch(version, 'python[23].*'):
