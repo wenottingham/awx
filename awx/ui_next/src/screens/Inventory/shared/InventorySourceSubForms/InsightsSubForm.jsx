413ae8e0b2e998bcc2d347f4f0d232e7712462c3
Bill Nottingham <notting@splat.cc>
2021-04-09 17:58:49 -0400

    Add UI bits

---
+++ awx/ui_next/src/screens/Inventory/shared/InventorySourceSubForms/InsightsSubForm.jsx
@@ -0,0 +1,81 @@
+import React, { useCallback } from 'react';
+import { useField, useFormikContext } from 'formik';
+
+import { t, Trans } from '@lingui/macro';
+import CredentialLookup from '../../../../components/Lookup/CredentialLookup';
+import {
+  OptionsField,
+  VerbosityField,
+  EnabledVarField,
+  EnabledValueField,
+  HostFilterField,
+  SourceVarsField,
+} from './SharedFields';
+import { required } from '../../../../util/validators';
+import getDocsBaseUrl from '../../../../util/getDocsBaseUrl';
+import { useConfig } from '../../../../contexts/Config';
+
+const InsightsSubForm = ({ autoPopulateCredential }) => {
+  const { setFieldValue } = useFormikContext();
+  const [credentialField, credentialMeta, credentialHelpers] = useField({
+    name: 'credential',
+    validate: required(t`Select a value for this field`),
+  });
+  const config = useConfig();
+
+  const handleCredentialUpdate = useCallback(
+    value => {
+      setFieldValue('credential', value);
+    },
+    [setFieldValue]
+  );
+
+  const pluginLink = `${getDocsBaseUrl(
+    config
+  )}/html/userguide/inventories.html#inventory-plugins`;
+  const configLink =
+    'https://docs.ansible.com/ansible/latest/collections/redhatinsights/insights/insights_inventory.html';
+
+  return (
+    <>
+      <CredentialLookup
+        credentialTypeNamespace="insights"
+        label={t`Credential`}
+        helperTextInvalid={credentialMeta.error}
+        isValid={!credentialMeta.touched || !credentialMeta.error}
+        onBlur={() => credentialHelpers.setTouched()}
+        onChange={handleCredentialUpdate}
+        value={credentialField.value}
+        required
+        autoPopulate={autoPopulateCredential}
+      />
+      <VerbosityField />
+      <HostFilterField />
+      <EnabledVarField />
+      <EnabledValueField />
+      <OptionsField />
+      <SourceVarsField
+        popoverContent={
+          <>
+            <Trans>
+              Enter variables to configure the inventory source. For a detailed
+              description of how to configure this plugin, see{' '}
+              <a href={pluginLink} target="_blank" rel="noopener noreferrer">
+                Inventory Plugins
+              </a>{' '}
+              in the documentation and the{' '}
+              <a href={configLink} target="_blank" rel="noopener noreferrer">
+                Insights
+              </a>{' '}
+              plugin configuration guide.
+            </Trans>
+            <br />
+            <br />
+          </>
+        }
+      />
+    </>
+  );
+};
+
+export default InsightsSubForm;
